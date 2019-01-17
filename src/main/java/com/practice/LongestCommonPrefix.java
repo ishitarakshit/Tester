5ccbde4e88eac3be75ec6f36ac954cf235e3961e
Ishita Rakshit <ishitarakshit@PIs-MacBook-Pro.local>
2019-01-16 21:49:41 -0500

    Shorter solution longest common prefix

--- src/main/java/com/practice/LongestCommonPrefix.java
+++ src/main/java/com/practice/LongestCommonPrefix.java
@@ -2,40 +2,13 @@ package com.practice;
 
 public class LongestCommonPrefix {
 	public String longestCommonPrefix(String[] strs) {
-		String lcp = "";
-		if(!isEmptyArray(strs)) {
-			String first = strs[0];
-			if(strs.length == 1) {
-				return first;
-			}
-			for(int i = 1;i <= first.length(); i++) {
-				String firstPrefix = first.substring(0, i);
-				for(int j = 1; j < strs.length; j ++) {
-					System.out.println("indexOf : "+strs[j].indexOf(first));
-					if(strs[j].startsWith(firstPrefix)) {
-						continue;
-					} else {
-						return lcp;
-					}
-				}
-				lcp = firstPrefix;
-			}
-		}
-		return lcp;
-	}
-	
-	public boolean isEmptyArray(String[] strs) {
-		if(strs.length > 0) {
-			for(String str:strs) {
-				 if(str.isEmpty()) {
-					 return true;
-				 } else {
-					 continue;
-				 }
-			}
-		} else {
-			return true;
-		}
-		return false;
+		if (strs.length == 0) return "";
+	    String prefix = strs[0];
+	    for (int i = 1; i < strs.length; i++)
+	        while (strs[i].indexOf(prefix) != 0) {
+	            prefix = prefix.substring(0, prefix.length() - 1);
+	            if (prefix.isEmpty()) return "";
+	        }        
+	    return prefix;
 	}
 }
